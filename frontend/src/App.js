ffde8bae0b72277dbe9b99b9c6106bbb508d56e0
Angel <nglbelangel@gmail.com>
2024-06-09 02:59:47 +0800

    replaceswitch with routes router version issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 import Navbar from './components/Layout/Navbar';
 import Footer from './components/Layout/Footer';
 import Home from './pages/Home';
@@ -13,11 +13,11 @@ const App = () => {
       <div>
         <Navbar />
         <Routes>
-          <Route exact path="/" component={Home} />
-          <Route path="/login" component={LoginPage} />
-          <Route path="/register" component={RegisterPage} />
-          <Route path="/logout" component={LogoutPage} />
-        </Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/logout" element={<LogoutPage />} />
+        </Routes> 
         <Footer />
       </div>
     </Router>
